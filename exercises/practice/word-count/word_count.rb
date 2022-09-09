a2f25f01d72b3a9da047967d50b229d4e5cf581d
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-09-08 20:23:02 -0500

    Complete word-count exercise

--- exercises/practice/word-count/word_count.rb
+++ exercises/practice/word-count/word_count.rb
@@ -5,3 +5,24 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+class Phrase
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def word_count
+        word_counts = {}
+        words = @word.gsub(/[^a-z0-9']/i," ").downcase.split(" ")
+        puts(words)
+        words.each do |word|
+            word.gsub!(/^'|'$/,"")
+            if !word_counts.key?(word)
+                word_counts[word] = 1
+            else
+                word_counts[word] += 1
+            end
+        end
+        word_counts
+    end
+end
